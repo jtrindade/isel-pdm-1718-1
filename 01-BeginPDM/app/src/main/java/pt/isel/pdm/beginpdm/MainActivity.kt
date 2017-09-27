99c504300106903307dd66f5814933ef8fffe891
Joao Trindade <jtrindade@cc.isel.ipl.pt>
2017-09-27 21:24:12 +0100

    First changes

--- 01-BeginPDM/app/src/main/java/pt/isel/pdm/beginpdm/MainActivity.kt
+++ 01-BeginPDM/app/src/main/java/pt/isel/pdm/beginpdm/MainActivity.kt
@@ -2,11 +2,24 @@ package pt.isel.pdm.beginpdm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var textBox : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textBox = findViewById(R.id.textBox) as TextView?
+    }
+
+    public fun onDoSomething(view: View?) {
+        val msg = "Message: " + (textBox?.getText() ?: "NONE")
+        Log.i("pdm/begin/main", msg)
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
