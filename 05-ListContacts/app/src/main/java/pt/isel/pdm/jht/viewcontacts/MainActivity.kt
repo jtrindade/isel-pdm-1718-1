7d76589c8d86ae7c9b705e98d6cc07516cdcd22e
Joao Trindade <jtrindade@cc.isel.ipl.pt>
2017-11-03 19:11:10 +0000

    Uses a Loader

--- 05-ListContacts/app/src/main/java/pt/isel/pdm/jht/viewcontacts/MainActivity.kt
+++ 05-ListContacts/app/src/main/java/pt/isel/pdm/jht/viewcontacts/MainActivity.kt
@@ -1,6 +1,9 @@
 package pt.isel.pdm.jht.viewcontacts
 
 import android.Manifest
+import android.app.LoaderManager
+import android.content.CursorLoader
+import android.content.Loader
 import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -8,19 +11,21 @@ import android.provider.ContactsContract
 import android.widget.TextView
 import android.content.pm.PackageManager
 import android.support.v4.content.ContextCompat
+import android.widget.Toast
 
 
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
 
-class MainActivity : AppCompatActivity() {
+    private val CONTACTS_LOADER = 88
 
     private val txtBox by lazy { findViewById(R.id.txtBox) as TextView }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Toast.makeText(this, "Create", Toast.LENGTH_LONG).show()
         if (hasPermissionToReadContacts())
-            loadContacts()
+            loaderManager.initLoader(CONTACTS_LOADER, null, this)
         else
             txtBox.text = "[NO PERMISSION TO READ CONTACTS]"
     }
@@ -29,23 +34,36 @@ class MainActivity : AppCompatActivity() {
         ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) ==
             PackageManager.PERMISSION_GRANTED
 
-    private fun loadContacts() {
-        var contactsCursor: Cursor = readContacts()
-        showContacts(contactsCursor)
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
+            when (id) {
+                CONTACTS_LOADER ->
+                    CursorLoader(
+                        this,
+                        ContactsContract.Contacts.CONTENT_URI,
+                        arrayOf(
+                                ContactsContract.Contacts._ID,
+                                ContactsContract.Contacts.DISPLAY_NAME,
+                                ContactsContract.Contacts.LOOKUP_KEY
+                        ),
+                        ContactsContract.Contacts.IN_VISIBLE_GROUP + "=1",
+                        null,
+                        ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+                    )
+                else ->
+                    throw IllegalArgumentException("unknown id: $id")
+            }
+
+    override fun onLoadFinished(loader: Loader<Cursor>?, data: Cursor?) {
+        Toast.makeText(this, "Load Finished", Toast.LENGTH_LONG).show();
+        if (data != null) {
+            showContacts(data)
+        }
     }
 
-    private fun readContacts(): Cursor =
-        contentResolver.query(
-            ContactsContract.Contacts.CONTENT_URI,
-            arrayOf(
-                ContactsContract.Contacts._ID,
-                ContactsContract.Contacts.DISPLAY_NAME,
-                ContactsContract.Contacts.LOOKUP_KEY
-            ),
-            ContactsContract.Contacts.IN_VISIBLE_GROUP + "=1",
-            null,
-            ContactsContract.Contacts.DISPLAY_NAME + " ASC"
-        )
+    override fun onLoaderReset(loader: Loader<Cursor>?) {
+        Toast.makeText(this, "Loader Reset", Toast.LENGTH_LONG).show();
+        txtBox.text = "[NO CONTACTS]"
+    }
 
     private fun showContacts(contactsCursor: Cursor) {
         if (contactsCursor.count > 0) {
